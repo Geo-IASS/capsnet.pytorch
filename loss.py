5465ad180e0f1b06bab4aa2d6beb29e26632bb80
Andrea Azzini <and.azzini@gmail.com>
2017-10-30 10:03:19 +0100

    Add digit margin loss

---
+++ loss.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class DigitMarginLoss(nn.Module):
+    def __init__(self, M=0.9, m=0.1, l=0.5):
+        super(DigitMarginLoss, self).__init__()
+        self.M = M
+        self.m = m
+        self.l = l
+
+    def forward(self, output, target):
+        norm = output.norm(dim=1)
+        zero = Variable(torch.zeros(1))
+        losses = [torch.max(zero, self.M - norm).pow(2) if digit == target.data[0]
+            else self.l * torch.max(zero, norm - self.m).pow(2)
+            for digit in range(10)]
+        return torch.cat(losses).sum()
--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from torchvision import datasets
 
 import transforms
 from capsnet import CapsNet
+from loss import DigitMarginLoss
 
 
 train_loader = torch.utils.data.DataLoader(
@@ -20,17 +21,21 @@ test_loader = torch.utils.data.DataLoader(
 
 model = CapsNet()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+criterion = DigitMarginLoss()
 model.train()
 
-for epoch, (input, target) in enumerate(train_loader, 1):
-    input = Variable(input)
-    target = Variable(target)
-
-    optimizer.zero_grad()
-
-    output = model(input)
-    criterion = torch.nn.MultiMarginLoss(2, 0.9, size_average=False)
-    loss = criterion(output.norm(dim=1), target)
-    loss.backward(retain_graph=True)
-    optimizer.step()
-    print(loss)
+for epoch in range(1, 11):
+    epoch_tot_loss = 0
+    for batch, (input, target) in enumerate(train_loader, 1):
+        input = Variable(input)
+        target = Variable(target)
+    
+        output = model(input)
+        loss = criterion(output, target)
+        epoch_tot_loss += loss
+        
+        optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        optimizer.step()
+        
+        print(loss.data[0], (epoch_tot_loss / batch).data[0])
